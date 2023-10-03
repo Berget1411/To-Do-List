f933beb400cf5a878fb5bd404a28db638e84e4f7
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2023-10-03 09:26:20 +0200

    add method to get formatted date

--- src/modules/task.js
+++ src/modules/task.js
@@ -31,6 +31,13 @@ export default class Task {
     return this.dueDate;
   }
 
+  getDateFormatted() {
+    const day = this.dueDate.split("/")[0];
+    const month = this.dueDate.split("/")[1];
+    const year = this.dueDate.split("/")[2];
+    return `${month}/${day}/${year}`;
+  }
+
   setPriority(priority) {
     this.priority = priority;
   }
