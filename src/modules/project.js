3d4b4de6668ab2f161785b6a1496d0e25657a308
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2023-10-04 12:58:48 +0200

    get task from today and week from project

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,3 +1,5 @@
+import { toDate, isToday, isThisWeek, subDays } from "date-fns";
+
 export default class Project {
   constructor(name) {
     this.name = name;
@@ -33,5 +35,18 @@ export default class Project {
       (task) => task.getTitle() === taskTitle
     );
     this.tasks.splice(this.tasks.indexOf(taskToRemove));
+  }
+
+  getTasksToday() {
+    return this.tasks.filter((task) => {
+      const taskDate = new Date(task.getDateFormatted());
+      return isToday(toDate(taskDate));
+    });
+  }
+  getTasksThisWeek() {
+    return this.tasks.filter((task) => {
+      const taskDate = new Date(task.getDateFormatted());
+      return isThisWeek(subDays(taskDate), 1);
+    });
   }
 }
